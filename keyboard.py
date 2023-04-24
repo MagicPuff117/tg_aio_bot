9c0822c7b4d5da0f0272cc14e7dc9f68c887caa7
Sergey Chernyshov <sergey@chernyshov117.ru>
2023-04-24 18:58:13 +0300

    fix

--- keyboard.py
+++ keyboard.py
@@ -1,15 +1,15 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 
 button_weather = InlineKeyboardButton('Weather', callback_data='weather')
 button_wind = InlineKeyboardButton('Wind', callback_data='wind')
 button_sun_time = InlineKeyboardButton('Sunrise and Sunset', callback_data='sun_time')
-button_currency = InlineKeyboardButton('Currency convert', callback_data='convert')
 
-WEATHER = InlineKeyboardMarkup().add(button_wind, button_sun_time, button_currency)
+
+WEATHER = InlineKeyboardMarkup().add(button_wind, button_sun_time)
 WIND = InlineKeyboardMarkup().add(button_weather).add(button_sun_time)
 SUN_TIME = InlineKeyboardMarkup().add(button_weather, button_wind)
 HELP = InlineKeyboardMarkup().add(button_weather, button_wind).add(button_sun_time)
-CONVERT = InlineKeyboardMarkup().add(button_currency)
+
 
 
